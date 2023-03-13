ded0310b106d791f47d56a2fdcbf934a8194ae5c
hwilliams-bod <herb.williams@boden.co.uk>
2023-03-13 13:14:32 +0000

    http server res value

--- app.js
+++ app.js
@@ -15,16 +15,16 @@ const server = http.createServer();
 server.on('request', async (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     const openai = new OpenAIApi(configuration);
-    const response = await openai.createCompletion({
+    const completion = await openai.createCompletion({
         model: "text-davinci-003",
-         prompt: "",
+         prompt: `Can you provide a link to a pair of shoes from Boden that would go well with this dress - https://www.boden.co.uk/en-gb/ruched-bodice-midi-dress-ivory-wild-bluebell/sty-d0298-ivo?cat=C1_S2_G4`,
         temperature: 0.7,
         max_tokens: 256,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
     });
-res.end(response);
+res.end(completion.data.choices[0].text);
 });
 
-server.listen();
+server.listen(3333);
